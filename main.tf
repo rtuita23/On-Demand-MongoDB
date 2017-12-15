6da6399886cfaa7f7c85a5279bd65ac364e00c1c
Ramsay Tuita <rktuita23@hotmail.com>
2017-12-14 22:47:46 -0800

    latest update

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ provider "aws" {
 
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    #region     = "us-east-1"
-    region      = "${var.aws_region}"
+    region     = "${var.aws_region}"
 }
 
 resource "aws_security_group" "instance" {
